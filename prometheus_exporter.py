33a14b7cc5783a32ec6a7ea418bf41f087161d94
Hamid Reza Moallem <mr.t3acher@gmail.com>
2025-06-21 16:58:41 +0330

    [feature] multiple exporter instance with one http_sd_config

---
+++ prometheus_exporter.py
@@ -0,0 +1,88 @@
+from flask import Flask, Response
+import waitress
+import os
+import threading
+
+from metrics import (
+    MetricSniffer,
+    TcpConnection,
+    TcpConnectionMetrics,
+    Metric,
+    MetricHeader,
+)
+
+
+class Exporter:
+    def __init__(self, name, listen_port=5000, debug=False, packet_filter=""):
+        self.name = name
+        self.listen_port = listen_port
+        self.debug = debug
+        self.packet_filter = (
+            "tcp" if packet_filter == "" else f"tcp and {packet_filter}"
+        )
+
+        self._setup_flask()
+
+    def _setup_flask(self):
+        self.app = Flask(self.name)
+        
+        # urls
+        self.app.add_url_rule(rule="/metrics", view_func=self.metrics)
+
+    def _thread_target(self, *args, **kwargs):
+        waitress.serve(self.app, *args, **kwargs)
+
+    def run(self, thread=False):
+        self.sniffer = MetricSniffer(filter=self.packet_filter)
+        kwargs = {"host": "0.0.0.0", "port": self.listen_port}
+        if thread:
+            self.thread = threading.Thread(name=self.name, target=self._thread_target, kwargs=kwargs)
+            self.thread.start()
+        else:
+            self.app.run(debug=self.debug, **kwargs)
+            
+    def join(self):
+        self.thread.join()
+        
+    def stop(self):
+        self.thread._stop()
+
+    def metrics(self):
+        """Endpoint to serve metrics in Prometheus format."""
+        classified_metrics = {}
+        for tcp_connection in self.sniffer.metrics:
+            tcp_connection: TcpConnection = tcp_connection
+            metrics: TcpConnectionMetrics = self.sniffer.metrics[tcp_connection]
+
+            metrics = metrics.get_metrics()
+            for header in metrics:
+                if header not in classified_metrics:
+                    classified_metrics[header] = []
+                classified_metrics[header].append(
+                    Metric(
+                        header.name,
+                        metrics[header],
+                        {
+                            "name": self.name,
+                            "src_ip": tcp_connection.src_ip,
+                            "src_port": tcp_connection.src_port,
+                            "dst_ip": tcp_connection.dst_ip,
+                            "dst_port": tcp_connection.dst_port,
+                        },
+                    )
+                )
+        response = ""
+        for header in classified_metrics:
+            response += str(header)
+            for metric in classified_metrics[header]:
+                response += str(metric)
+        return Response(response, mimetype="text/plain")
+
+
+if __name__ == "__main__":
+    LISTEN_PORT = os.environ.get("LISTEN_PORT", "5000")
+    DEBUG = bool(os.environ.get("DEBUG", "0"))
+    PACKET_FILTER = os.environ.get("PACKET_FILTER", "tcp")
+
+    exporter = Exporter(LISTEN_PORT, DEBUG, PACKET_FILTER)
+    exporter.run()
---
+++ prometheus_http_sd.py
@@ -0,0 +1,78 @@
+from flask import Flask, Response
+import waitress
+import netifaces
+import os
+import json
+
+from prometheus_exporter import Exporter
+
+
+class ExporterSet:
+    def __init__(self, listen_host="0.0.0.0", listen_port=5000, debug=False, external_host=""):
+        self.listen_host = listen_host
+        self.listen_port = listen_port
+        self.next_exporter_listen_port = listen_port + 1
+        self.debug = debug
+        self.external_host = (
+            external_host if external_host != "" else self._get_primary_ip()
+        )
+
+        self.exporters = []
+
+        self._setup_flask()
+
+    def _get_primary_ip(self):
+        interfaces = netifaces.interfaces()
+        for interface in interfaces:
+            # Get the addresses associated with the interface
+            addresses = netifaces.ifaddresses(interface)
+            # Check if the interface has an IPv4 address
+            if netifaces.AF_INET in addresses:
+                ip_address = addresses[netifaces.AF_INET][0]["addr"]
+                # Ignore loopback addresses
+                if not ip_address.startswith("127."):
+                    return ip_address
+        return "127.0.0.1"  # No valid IP found
+
+    def _setup_flask(self):
+        self.app = Flask(__name__)
+
+        # urls
+        self.app.add_url_rule(rule="/sd", view_func=self.sd)
+
+    def run(self):
+        for exporter in self.exporters:
+            exporter.run(thread=True)
+
+        waitress.serve(self.app, host=self.listen_host, port=self.listen_port)
+
+    def sd(self):
+        response = {}
+        response["targets"] = [
+            f"{self.external_host}:{exporter.listen_port}"
+            for exporter in self.exporters
+        ]
+        return Response(json.dumps(response), mimetype="application/json")
+
+    def new_exporter(self, name, packet_filter):
+        exporter = Exporter(
+            name, self.next_exporter_listen_port, self.debug, packet_filter
+        )
+        self.exporters.append(exporter)
+        self.next_exporter_listen_port += 1
+
+
+if __name__ == "__main__":
+    LISTEN_HOST = os.environ.get("LISTEN_hHOST", "0.0.0.0")
+    LISTEN_PORT = int(os.environ.get("LISTEN_PORT", "5000"))
+    DEBUG = bool(os.environ.get("DEBUG", "0"))
+    EXTERNAL_HOST = os.environ.get("EXTERNAL_HOST", "")
+
+    exporter_set = ExporterSet(
+        listen_host=LISTEN_HOST, listen_port=LISTEN_PORT, debug=DEBUG, external_host=EXTERNAL_HOST
+    )
+    for name, value in os.environ.items():
+        if name.startswith("PACKET_FILTER_"):
+            name = name.removeprefix("PACKET_FILTER_")
+            exporter_set.new_exporter(name, value)
+    exporter_set.run()
